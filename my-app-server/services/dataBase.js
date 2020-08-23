5e52ae28eb25dab6e22a97eb6a016b5863cfaaa8
shimon tadesa <shimontadesa123@gmail.com>
2020-08-23 18:29:29 +0300

    change db config

--- my-app-server/services/dataBase.js
+++ my-app-server/services/dataBase.js
@@ -5,13 +5,14 @@ const gconfig = global.gConfig;
 
 
 function connect(cb) {
-  mongoose.connect(gconfig.db_mongo, { useNewUrlParser: true }).then(
+  console.log("db address is :"+gconfig.dbHost);
+  mongoose.connect(gconfig.dbHost, { useNewUrlParser: true }).then(
     (res) => {
 
       console.log("connected to data base, all is good!");
       cb(res);
     }, err => {
-      console.log("error connecting to the data base");
+      console.log("error connecting to the data base" ,err);
       
     });
 
