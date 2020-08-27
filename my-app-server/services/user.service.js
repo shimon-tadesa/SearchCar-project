9d2d1beb9c3431e371cde80f2f7a62ebf61cc871
shimon-tadesa <shimontadesa123@gmail.com>
2020-08-27 15:32:24 +0300

    update and clean user service

--- my-app-server/services/user.service.js
+++ my-app-server/services/user.service.js
@@ -3,13 +3,14 @@ const jwt = require("jsonwebtoken");
 const axios = require("axios");
 const gConfig = global.gConfig;
 
-function signToken(data) {
+// create jwt token from object
+function createToken(data) {
   return jwt.sign(data, gConfig.jwtSecret, { expiresIn: "3d" });
 }
 
+// create jwt token from user object ,and return token with user details
 function signUser(user) {
-   
-  let tokenString = signToken({...user});
+  let tokenString = createToken({ ...user });
   let tokenExpiration = new Date();
   tokenExpiration.setDate(tokenExpiration.getDate() + 3);
   return {
@@ -19,6 +20,7 @@ function signUser(user) {
   };
 }
 
+// register new user
 async function register(newUser) {
   try {
     const user = new UserModel(newUser);
@@ -45,96 +47,87 @@ async function loginLocal(umail, pass) {
       throw "error bad password";
     }
   } catch (error) {
-    if(error=="error bad password"){
+    if (error == "error bad password") {
       throw "error bad password";
     }
     throw "error login user";
   }
 }
 
-function verifyOauth(email,emailFromToken){
-  return (email == emailFromToken);
+// login extrnal users e.g facebook google
+// if user doesnt exist in db than register it
+async function onOauthLogin(user) {
+  const dbUser = await UserModel.findOne({ email: user.email });
+  let userObj;
+  // if doesnt exist  create user
+  if (!dbUser) {
+    await register(user);
+  } else {
+    //should remove id from response
+    userObj = dbUser.toJSON();
+    delete userObj._id;
+    delete userObj.__v;
+  }
+
+  //sign in user
+  let resData = signUser(userObj);
+  return resData;
 }
 
-async function onOauthLogin (user) {
-   
-    const dbUser = await UserModel.findOne({ email: user.email });
-    let userObj;
-    if (!dbUser) {
-      //  create user
-      userObj = {
-          email:user.email,
-          type:user.type,
-          familyName:user.familyName,
-          firstName: user.firstName,
-          type:user.type,
-          imageUrl:user.imageUrl
-      }
-      await register(userObj);
-       
-    }else{
-        //should remove id from response
-      userObj =  dbUser.toJSON();
-      delete userObj._id;
-      delete userObj.__v
-    }
-
-    //sign in user
-    let resData = signUser(userObj);
-    return resData;
-}
-
-
-async function loginGoogle(user) {
+// get google user details using accsess token
+async function loginGoogle(accessToken) {
   try {
-    const res = await axios.get(
-      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token="${user.accessToken}"`
+    //get user details
+    const { data } = await axios.get(
+      `https://www.googleapis.com/oauth2/v3/userinfo?access_token="${accessToken}"`
     );
 
-    if(verifyOauth(user.email,res.data.email)){
-    return  onOauthLogin(user);
-    }
-    else{
-      throw "unauthorized user";
-    }
-
+    const user = {
+      email: data.email,
+      type: "google",
+      familyName: data.family_name,
+      firstName: data.given_name,
+      imageUrl: data.picture,
+    };
+    return onOauthLogin(user);
   } catch (error) {
     throw "failed to login with google account";
   }
 }
 
-async function loginFacebook(user) {
+// get facebook user details using accsess token and user id
+async function loginFacebook(userId, accessToken) {
   try {
-    const res = await axios.get(
-      `https://graph.facebook.com/${user.id}?fields=email&access_token=${user.accessToken}`
+    //get facebook user details
+    const { data } = await axios.get(
+      `https://graph.facebook.com/${userId}?fields=id,name,first_name,last_name,email,picture{url}&access_token=${accessToken}`
     );
 
-    if(verifyOauth(user.email,res.data.email)){
-      return  onOauthLogin(user);
-    }
-    else{
-      throw "unauthorized user";
-    }
-    
-  }catch(err){
+    const user = {
+      email: data.email,
+      type: "facebook",
+      familyName: data.last_name,
+      firstName: data.first_name,
+      imageUrl: data.picture.data.url,
+    };
+    // login  facebook user
+    return onOauthLogin(user);
+  } catch (err) {
     throw "error validating user!";
   }
 }
-
- 
 
 async function login(user) {
   if (user.type == "local") {
     return await loginLocal(user.email, user.password);
   } else if (user.type == "google") {
-    return await loginGoogle(user);
-  }else if(user.type=="facebook"){
-    return await loginFacebook(user);
+    return await loginGoogle(user.accessToken);
+  } else if (user.type == "facebook") {
+    return await loginFacebook(user.id, user.accessToken);
   }
 }
 
 module.exports = {
   register,
   login,
-  signUser
 };
